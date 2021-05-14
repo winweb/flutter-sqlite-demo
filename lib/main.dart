9154fcded32eaebacdd2c9842c89e4f0efb60986
theerasun_tangngamchit <theerasun@gmail.com>
2021-05-14 21:54:22 +0700

    -makes update task popup. -show TODO Checked List.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
 
 		return MaterialApp(
 			title: 'Flutter Demo',
-			theme: ThemeData( primarySwatch: Colors.indigo ),
+			theme: ThemeData(primarySwatch: Colors.indigo ),
 			home: MyHomePage(title: 'Flutter SQLite Demo App'),
 		);
 	}
@@ -49,15 +49,21 @@ class _MyHomePageState extends State<MyHomePage> {
 			key: Key(item.id.toString()),
 			child: Padding(
 				padding: EdgeInsets.fromLTRB(12, 6, 12, 4),
-				child: FlatButton(
+				child: TextButton(
 					child: Row(
 						mainAxisAlignment: MainAxisAlignment.spaceBetween,
 						children: <Widget>[
 							Text(item.task, style: _style),
-							Icon(item.complete == true ? Icons.radio_button_checked : Icons.radio_button_unchecked, color: Colors.white)
+							IconButton(
+								icon: Icon(item.complete == true ? Icons.radio_button_checked : Icons.radio_button_unchecked, color: Colors.white),
+								tooltip: 'check',
+								onPressed: () {
+									_toggle(item);
+								},
+							),
 						]
 					),
-					onPressed: () => _toggle(item),
+					onPressed: () => _edit(context, item),
 				)
 			),
 			onDismissed: (DismissDirection direction) => _delete(item),
@@ -118,6 +124,41 @@ class _MyHomePageState extends State<MyHomePage> {
 		);
 	}
 
+	void _update(TodoItem item) async {
+
+		Navigator.of(context).pop();
+
+		await DB.update(TodoItem.table, item);
+		refresh();
+	}
+
+	void _edit(BuildContext context, TodoItem item) {
+
+		showDialog(
+				context: context,
+				builder: (BuildContext context) {
+					return AlertDialog(
+						title: Text("Edit Task"),
+						actions: <Widget>[
+							TextButton(
+									child: Text('Cancel'),
+									onPressed: () => Navigator.of(context).pop()
+							),
+							TextButton(
+									child: Text('Update'),
+									onPressed: () => _update(item)
+							)
+						],
+						content: TextField(
+							autofocus: true,
+							decoration: InputDecoration(labelText: 'Task Name', hintText: 'e.g. pick up bread'),
+							onChanged: (value) { item.task = value; },
+						),
+					);
+				}
+		);
+	}
+
 	@override
 	void initState() {
 
@@ -141,10 +182,93 @@ class _MyHomePageState extends State<MyHomePage> {
 			body: Center(
 				child: ListView( children: _items )
 			),
-			floatingActionButton: FloatingActionButton(
-				onPressed: () { _create(context); },
-				tooltip: 'New TODO',
-				child: Icon(Icons.library_add),
+			floatingActionButton: Column(
+				mainAxisAlignment: MainAxisAlignment.end,
+				children: [
+					FloatingActionButton(
+						onPressed: () { _create(context); },
+						tooltip: 'New TODO',
+						child: Icon(Icons.library_add),
+						heroTag: null
+					),
+					SizedBox(
+						height: 5,
+					),
+					FloatingActionButton(
+						onPressed: () {
+							Navigator.push(
+								context,
+								MaterialPageRoute(
+									builder: (context) => SecondRoute(_tasks.where((map)=>map.complete == true).toList())
+								),
+							);
+						},
+						tooltip: 'Get TODO checked',
+						child: Icon(Icons.library_books),
+						heroTag: null
+					)
+				]
+			)
+		);
+	}
+
+	void _showDialog(BuildContext context) {
+		showDialog(
+			context: context,
+			builder: (BuildContext context) {
+				return AlertDialog(
+					title: new Text("Alert!!"),
+					content: new Text("You are awesome!"),
+					actions: <Widget>[
+						new TextButton(
+							child: new Text("OK"),
+							onPressed: () {
+								Navigator.of(context).pop();
+							},
+						),
+					],
+				);
+			},
+		);
+	}
+}
+
+class SecondRoute extends StatelessWidget {
+
+	List<TodoItem> _results;
+
+	SecondRoute(this._results);
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text("TODO Checked List"),
+			),
+			body:
+				Column(
+				mainAxisAlignment: MainAxisAlignment.spaceBetween,
+				children: <Widget>[
+					new Expanded (
+						child: new ListView.builder(
+							scrollDirection: Axis.vertical,
+							shrinkWrap: true,
+							itemCount: _results.length,
+							itemBuilder: (BuildContext ctxt, int index) {
+								return new Text(_results[index].task, style: TextStyle(color: Colors.red, fontSize: 24));
+							}
+						)
+					),
+					SizedBox(
+						height: 30,
+					),
+					ElevatedButton(
+						onPressed: () {
+							Navigator.pop(context);
+						},
+						child: Text('Go back!'),
+					)
+				],
 			)
 		);
 	}
